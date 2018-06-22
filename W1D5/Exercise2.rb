8ed58cc31617f6c484341f12e67ca23aedcda193
aknishi <adriankawanishi@gmail.com>
2018-06-22 01:50:16 -0700

    Exercise 2 - Queue

---
+++ W1D5/Exercise2.rb
@@ -0,0 +1,19 @@
+class Queue
+
+  def initialize
+    @array = []
+  end
+
+  def enqueue(el)
+    @array.push(el)
+  end
+
+  def dequeue
+    @array.shift
+  end
+
+  def peek
+    @array.first
+  end
+
+end
