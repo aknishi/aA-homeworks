a4546fad48434bdec5c70066321c6f52b77e5973
aknishi <adriankawanishi@gmail.com>
2018-06-23 08:20:27 -0700

    Add Simon homework

---
+++ W2D1/Simon/lib/simon.rb
@@ -0,0 +1,62 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    until @game_over == true
+      take_turn
+      system("clear")
+    end
+
+    game_over_message
+    reset_game
+  end
+
+  def take_turn
+    show_sequence
+    guess = require_sequence
+    unless game_over
+      round_success_message
+      @sequence_length += 1
+    end
+    @game_over = true unless @seq == guess
+  end
+
+  def show_sequence
+    add_random_color
+    @seq.each {|color| puts color}
+    sleep 0.75
+    system("clear")
+    sleep 0.25
+  end
+
+  def require_sequence
+    print "Enter sequence: (ex: r, b, g, y)"
+    input_seq = gets.chomp.split(",")
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+  end
+
+  def round_success_message
+    print "Great job!"
+  end
+
+  def game_over_message
+    "That is not the right sequence. You got to #{sequence_length} rounds"
+  end
+
+  def reset_game
+    @seq = []
+    @sequence_length = 1
+    @game_over = false
+  end
+end
